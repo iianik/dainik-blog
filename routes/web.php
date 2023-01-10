b2bfba9b673c14c7cab83e0699e29c6d00d1fe9f
Anik Dey <57797374+iianik@users.noreply.github.com>
2023-01-10 11:58:55 +0600

    Start Frontend

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('frontend.frontend-master');
-});
+// Route::get('/', function () {
+//     return view('frontend.frontend-master');
+// });
+Route::get('/', [HomeController::class, 'index']);
+
 
 Route::middleware([
     'auth:sanctum',
